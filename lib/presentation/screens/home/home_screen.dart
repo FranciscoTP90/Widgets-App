8628fd4215b56ddf4592fa78b00e1b7572124202
FranciscoTP90 <franciscotp90@gmail.com>
2024-02-27 18:13:24 -0600

    home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,9 +10,50 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Flutter + Material 3"),
       ),
-      body: const Center(
-        child: Text('HomeScreen'),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Builder en tiempo de ejecucion
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTile(menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile(this.menuItem);
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final MenuItem(:title, :subtitle, :link, :icon) = menuItem;
+
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: colorScheme.primary,
       ),
+      trailing: const Icon(Icons.arrow_forward_ios_rounded),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      onTap: () {
+        // link
+      },
     );
   }
 }
